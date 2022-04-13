073421048c94d81dee704f01490287b1a036af37
Christian Seip <christian.seip@ravitar.de>
2022-04-13 19:46:34 +0200

    New Questions added

--- js/db.js
+++ js/db.js
@@ -4,30 +4,60 @@ let questionPool = [
         "intro": "Welcome to the<br>JavaScript Quiz!",
         "questions": [
             {
-                "question": "Frage 1",
-                "options": ["A1", "A2", "A3", "A4"],
+                "question": "Wich HTML Element is used for JavaScript",
+                "options": ["&lt;javascript&gt;", "&lt;script&gt;", "&lt;js&gt;", "&lt;code&gt;"],
                 "a": 1
             },
             {
-                "question": "Frage 2",
-                "options": ["A1", "A2", "A3", "A4"],
+                "question": "Where is the correct place to add JavaScript?",
+                "options": ["&lt;body&gt; section", "&lt;head&gt; section", "Both, &lt;head&gt; and &lt;body&gt; section"],
+                "a": 2
+            },
+            {
+                "question": "What is the correct syntax to include an external Javascript?",
+                "options": ["&lt;script src=\"filename.js\"&gt;", "&lt;script href=\"filename.js\"&gt;", "&lt;script name=\"filename.js\"&gt;", "&lt;script include=\"filename.js\"&gt;"],
+                "a": 0
+            },
+            {
+                "question": "How did you call a function named 'doAnything'?",
+                "options": ["new doAnything()", "doAnything", "doAnything()", "function doAnything()"],
+                "a": 2
+            },
+            {
+                "question": "Wich one is an invalid if statement?",
+                "options": ["if(i == 0) { a = true; }", "if(i == 0) a = true;", "a = i == 5 ? true : false;", "if(i == 0) then a = true;"],
                 "a": 3
             },
         ]
     },
     {
-        "category": "HTML",
-        "intro": "Welcome to the<br>HTML Quiz!",
+        "category": "C#",
+        "intro": "Welcome to the<br>C# Quiz!",
         "questions": [
             {
-                "question": "Frage 1",
-                "options": ["A1", "A2", "A3", "A4"],
+                "question": "What is a correct syntax to write 'Hello World' in the console?",
+                "options": ["Console.WriteLine(\"Hello World\");", "print(\"Hello World\");", "System.out.println(\"Hello World\");", "cout << \"Hello World\";"],
+                "a": 0
+            },
+            {
+                "question": "Type conversion happens when we assign the value of one data type to another.",
+                "options": ["True", "False"],
+                "a": 0
+            },
+            {
+                "question": "What is the correct way to create an object called myObject of MyClass?",
+                "options": ["new myObject = MyClass();", "MyClass myObject = new MyClass();", "class MyObject = new MyClass();", "MyClass myObject = MyClass();"],
                 "a": 1
             },
             {
-                "question": "Frage 2",
-                "options": ["A1", "A2", "A3", "A4"],
+                "question": "What is the name of the special class that represents a group of constants?",
+                "options": ["void", "special", "const", "enum"],
                 "a": 3
+            },
+            {
+                "question": "How do you write comments in C# code?",
+                "options": ["/*", "//", "#", "&lt;!--"],
+                "a": 1
             },
         ]
     }
